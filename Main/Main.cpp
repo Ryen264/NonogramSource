3a0db4f9899e8ef70c5f256d8974f7a4596e4880
JulieDnqh <dnqh04@gmail.com>
2023-05-17 18:16:34 +0700

    Convert binary image to a matrix

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,7 +1,52 @@
-#include "Global.h"
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+using namespace std;
+using namespace cv;
+
+int** generateMat(const char* imagepath, int height, int width) {
+    int** mat = new int* [height];
+    for (int i = 0; i < height; i++)
+        mat[i] = new int[width];
+
+    Mat img = imread(imagepath);
+    if (img.empty()) {
+        cout << "Image is not successfully loaded!\n";
+        return mat;
+    }
+    imshow("Image", img);
+
+    int x = img.cols - 2 - (height - 1) - 12 * height - (height / 5 - 1),
+        y = img.rows - 2 - (width - 1) - 12 * width - (width / 5 - 1) - 10;
+
+    for (int x0 = x + 1, j0 = 0; j0 < width; x0 += 13, j0++)
+        mat[0][j0] = ((int)img.at<Vec3b>(Point(x0, y + 1))[0] < 2) ? 1 : -1;
+
+    for (int y0 = y + 1, i0 = 0; i0 < height; y0 += 13, i0++)
+        mat[i0][0] = ((int)img.at<Vec3b>(Point(x + 1, y0))[0] < 2) ? 1 : -1;
+
+    for (int j = 1; j < width; j++)
+        for (int i = 1; i < height; i++)
+            mat[i][j] = ((int)img.at<Vec3b>(Point(x + j * 13 + j / 5, y + i * 13 + i / 5))[0] < 2) ? 1 : -1;
+
+    return mat;
+}
 
 int main()
 {
-    cout << "Chao\n";
+    int height = 5, width = 5;
+    int** matrix = generateMat("krolik6_12_1_1p.png", height, width);
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+            if (matrix[i][j] == 1)
+                cout << "+";
+            else if (matrix[i][j] == -1)
+                cout << "-";
+            else
+                cout << matrix[i][j] << ' ';
+        cout << endl;
+    }
     return 0;
 }
